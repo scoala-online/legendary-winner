3675efb6f9cf7a2c9a7a412a67775265b75aa77a
Alexandru Istrate <38143225+AlexandruIstrate@users.noreply.github.com>
2021-04-09 00:29:01 +0300

    feat: 🎸 Remove stupid files
    Removed the stupid files I shouldn't have added

--- apps/java-api/src/main/java/org/scoalaonline/api/repo/StoreRepo.java
+++
@@ -1,8 +0,0 @@
-package org.scoalaonline.api.repo;
-
-import org.scoalaonline.api.model.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepo extends JpaRepository<Store, Long> {
-
-}
