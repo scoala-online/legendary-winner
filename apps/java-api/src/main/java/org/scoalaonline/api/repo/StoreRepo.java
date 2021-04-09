9b8c4a705e46d42867e39af35e48a90736a75507
Alexandru Istrate <38143225+AlexandruIstrate@users.noreply.github.com>
2021-04-09 13:55:29 +0300

    feat: 🎸 Add Store Repo and Service
    Added Store Repo and Service classes

---
+++ apps/java-api/src/main/java/org/scoalaonline/api/repo/StoreRepo.java
@@ -0,0 +1,7 @@
+package org.scoalaonline.api.repo;
+
+import org.scoalaonline.api.model.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepo extends JpaRepository<Store, Long> {
+}
