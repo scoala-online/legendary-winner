09b0d3794e2812d2b90c2205a8967aba7636c5c9
Bogdan <pacurarbogdanemil@gmail.com>
2021-04-12 23:59:44 +0300

    feat: 🎸 Add CustomerService

---
+++ apps/java-api/src/main/java/org/scoalaonline/api/service/CustomerService.java
@@ -0,0 +1,54 @@
+package org.scoalaonline.api.service;
+
+import org.scoalaonline.api.model.Customer;
+import org.scoalaonline.api.repo.CustomertRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerService {
+
+  @Autowired
+  private CustomertRepo customerRepo;
+
+  public List<Customer> getAllCustomers() {
+    return customerRepo.findAll();
+  }
+
+  public Optional<Customer> getCustomerById(long id) {
+    return customerRepo.findById(id);
+  }
+
+  public Customer addCustomer(Customer customer) {
+    // Clear the ID supplied by the user
+    customer.setCustomerID(0);
+
+    // Save the new customer
+    return customerRepo.save(customer);
+  }
+
+  public Customer updateCustomer(long id, Customer customer) {
+    Optional<Customer> customerToUpdate = customerRepo.findById(id);
+
+    // Check that the customer exists
+    if (customerToUpdate.isEmpty()) {
+      // If the customer with the given ID doesn't exist, return null
+      return null;
+    }
+
+    // Copy the ID of the existing object into the updated object
+    customer.setCustomerID(customerToUpdate.get().getCustomerID());
+
+    // Save the updated customer with the right ID so that the old one gets overwritten
+    return customerRepo.save(customer);
+  }
+
+  public void deleteCustomer(long id) {
+    customerRepo.deleteById(id);
+  }
+
+  public boolean customerExists(long id) {
+    return customerRepo.existsById(id);
+  }
+}
