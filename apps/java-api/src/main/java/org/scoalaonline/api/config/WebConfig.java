0117724b3348adfc31c529e25cad1ded843fd5c4
Alexandru Istrate <38143225+AlexandruIstrate@users.noreply.github.com>
2021-04-15 23:01:17 +0300

    fix: 🐛 Fix package name for WebConfig
    Fix package name for WebConfig

--- apps/java-api/src/main/java/org/scoalaonline/api/config/WebConfig.java
+++ apps/java-api/src/main/java/org/scoalaonline/api/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.programming.examproject.config;
+package org.scoalaonline.api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
